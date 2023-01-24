02e96bab5d2c1c3ac5c53f234b4f4771dab2e17a
EstefaniOviedo <estefani.oviedo20@gmail.com>
2023-01-24 16:35:48 -0600

    Add services

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,24 @@
+
+import diaryData from './diaries.json'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry, } from '../types'
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const findById = (id: number): DiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id)
+    return entry
+}
+export const getEntriesWithInputSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+    return diaries.map(({ id, date, weather, visibility }) => {
+        return {
+            id,
+            date,
+            weather,
+            visibility
+        }
+    })
+}
+
+export const addEntry = (): undefined => undefined
