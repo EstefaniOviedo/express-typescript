098015617cca896e74cbdb8def2f6d23e5c30823
EstefaniOviedo <estefani.oviedo20@gmail.com>
2023-01-25 00:43:11 -0600

    Add enum weather and visibility

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,20 @@
 export type Weather = 'sunny' | 'rainy' | 'cloudy' | ' windy' | 'stormy'
 export type Visibility = 'great' | 'good' | 'ok' | 'poor'
 
+export enum Weather {
+    Sunny = 'sunny' 
+    Rainy = 'rainy' 
+    Cloudy = 'cloudy'
+    Windy = ' windy' 
+    Stormy = 'stormy'
+}
+
+export enum Visibility {
+    Great ='great',
+    Good ='good',
+    Ok ='ok',
+    Poor ='poor'
+}
 export interface DiaryEntry {
     id: number
     date: string
@@ -12,3 +26,4 @@ export interface DiaryEntry {
 //export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, "id" | "date" | "weather" | "visibility">
 
 export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
